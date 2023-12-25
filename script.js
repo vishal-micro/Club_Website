a18b178de51b15d1e1aa3e49bdc223d1f006c834
Vishal Srivastav <vishal.micro2004@gmail.com>
2023-12-25 14:48:08 +0530

    adding new files

---
+++ script.js
@@ -0,0 +1,45 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+   for(tablink of tablinks){
+       tablink.classList.remove("active-link");
+   }
+   for(tabcontent of tabcontents){
+       tabcontent.classList.remove("active-tab");
+   }
+   event.currentTarget.classList.add("active-link");
+   document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+var sidemeu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemeu.style.right = "0";
+}
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
+
+
+
+
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycby1rJysbc3ub3HG4128K0svvnbFuGkW6j8Px0y8zohSV6TGU__0CCNkgbRgUCDe7fDt/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+          msg.innerHTML = "Message sent successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },3000)
+            form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
